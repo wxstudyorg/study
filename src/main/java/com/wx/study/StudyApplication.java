02579a6027a7485f0d09d319711549345d99c059
lijingling <m18715171408@163.comm>
2019-08-23 18:33:04 +0800

    添加String

--- src/main/java/com/wx/study/StudyApplication.java
+++ src/main/java/com/wx/study/StudyApplication.java
@@ -9,6 +9,8 @@ public class StudyApplication {
     public static void main(String[] args) {
         int m=1;
         System.out.println(m+1);
+        String str = "lijingling";
+        System.out.println(str);
         SpringApplication.run(StudyApplication.class, args);
     }
 
